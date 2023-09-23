c7ff3ec3c98d8601c417271ef1f3bc1eeee4ed5b
Jonathan <jonathanelian64@gmail.com>
2023-09-23 16:15:20 -0700

    feat: add view transitions api and about me

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,7 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://yourwebsiteurl.com";
+const LIVE_URL = "https://blog-jona.netlify.app/";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -30,3 +30,4 @@ export default defineConfig({
   site: BASE_URL,
   integrations: [sitemap()],
 });
+
